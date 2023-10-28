0d9a2fcfbea3d05a6876478a7becffca21f5b8af
Sonia Janota <sonjaaehm@gmail.com>
2023-10-28 23:43:34 +0200

    Add Snack and Drink class + polymorphism comment

--- item.py
+++ item.py
@@ -3,3 +3,23 @@ class Item:
         self.name = name
         self.price = price
         self.quantity = quantity
+
+    def is_cold(self):
+        return False
+
+class Drink(Item):
+    pass
+
+class Snack(Item):
+    def __init__(self, name, price, quantity, cold):
+        super().__init__(name, price, quantity)
+        self.cold = cold
+
+    def is_cold(self):
+        return self.cold
+
+# def use_item(item: Item):
+#     print(f"using {item.name} it is {'cold' if item.is_cold() else 'warm'}")
+
+# ice_cream = Snack("Ice Cream", "5zl", 20, True)
+# use_item(ice_cream)
