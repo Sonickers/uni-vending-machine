fd42eae401c319dee6bfd55f6788b765e34e54c0
Sonia Janota <sonjaaehm@gmail.com>
2023-12-17 12:21:32 +0100

    Decrease quantity of item

--- vending.py
+++ vending.py
@@ -19,7 +19,8 @@ class VendingMachine:
             print(f"Items price is: {self.money.format_value(item.price)}")
             coins_change = self.money.get_change(item.price, self.intake_money(item))
             print("Returning the change: ", coins_change)
-            # TODO: decrease item quantity by 1
+            item.quantity -= 1
+            print(item.quantity)
             # TODO: save current items,springs and money state to file -> example: self.data_updater.update(self.items, self.springs, self.money.stocked)
         else:
             print("Wrong product code. Please try again.")
